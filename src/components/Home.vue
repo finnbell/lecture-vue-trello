6ea68adcc441a440ac69fb7b42f7d394a523afa6
finn <finnbell@naver.com>
2023-11-22 23:54:29 +0900

    vuex state 에러 발생

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -20,7 +20,7 @@
 <script>
 import {board} from '../api'
 import AddBoard from './AddBoard.vue'
-
+import {mapState} from 'vuex'
 
 export default {
     components : {
@@ -31,8 +31,12 @@ export default {
             loading: false,
             boards: [],
             error: '',
-            isAddBoard: false
         }
+    },
+    computed: {
+        ...mapState([ 
+            'isAddBoard' 
+        ]),
     },
     created() {
         this.fetchData() 
@@ -54,7 +58,8 @@ export default {
                 })        
         },
         addBoard() {
-            this.isAddBoard = true
+            //this.isAddBoard = true
+
             console.log('addBoard()')
         },
         onAddBoard(title) {            
