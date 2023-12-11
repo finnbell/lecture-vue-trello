7341c1f8d52ca5f5bb48790d040fa0d8657287e0
finn <finnbell@naver.com>
2023-12-11 23:16:32 +0900

    보드 생성후 조회 board/query

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -4,6 +4,7 @@
     <div v-if="loading">loading board...</div>
     <div v-else>
       <div>bid: {{ bid }}</div>
+      <pre>{{ board }}</pre>
       <router-link :to="`/b/${bid}/c/1`">Card 1</router-link>
       <router-link :to="`/b/${bid}/c/2`">Card 2</router-link>
     </div>
@@ -12,7 +13,10 @@
   </div>
 </template>
 
+
 <script>
+import { mapState, mapActions } from 'vuex'
+
 export default {
     data() {
         return {
@@ -20,15 +24,24 @@ export default {
             loading: false
         }
     },
-
+    computed: {
+      ...mapState({
+        board: 'board'
+      })
+    },
+    created() {
+      this.fetchData()
+    },
     methods: {
+      ...mapActions([
+        'FETCH_BOARD'
+      ]),
+
       fetchData() { 
         this.loading = true        
+        this.FETCH_BOARD({id: this.$route.params.bid })
+          .then(() => this.loading = false)
 
-        setTimeout(() => {
-          this.bid = this.$route.params.bid
-          this.loading = false        
-        }, 500)
       }
     }
 
--- src/store/actions.js
+++ src/store/actions.js
@@ -6,13 +6,19 @@ const actions = {
             .then(( {acceccToken}) => commit('LOGIN', acceccToken))
     },
     ADD_BOARD(_, {title}) {
-        return api.board.create(title)
+        return api.board.create(title).then(data => data.item)
     },
     FETCH_BOARDS({ commit }) {
         return api.board.fetch().then(data => {
             commit('SET_BOARDS', data.list )
         })
+    },
+    FETCH_BOARD({commit}, {id}) {
+        return api.board.fetch(id).then( data => {
+            commit('SET_BOARD', data.item)
+        })
     }
+
 }
 
 export default actions
