1c8c2b68c587ecc01ef8557d8d54bba861151700
finn <finnbell@naver.com>
2024-01-13 18:11:53 +0900

    카드 삭제 기능 구현

--- src/api/index.js
+++ src/api/index.js
@@ -48,7 +48,11 @@ export const card = {
     },
     update(id, payload) {
         return request('put', `/cards/${id}`, payload)
-    }
+    },
+    destroy(id) {
+        return request('delete',`/cards/${id}` )
+    },
+
 
 }
 
